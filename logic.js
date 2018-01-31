a58963113c61553cb6333652e8d01a1c9e2e2955
ymekias1223 <yusuf.mekias@gmail.com>
2018-01-30 22:04:26 -0800

    thrid commit, getting there

--- logic.js
+++ logic.js
@@ -1,54 +1,46 @@
-var topics = ["Chocolate Rain", "Nyan Cat", "Doge","MLG","Bad Luck Brian" ]
-var element_itterator = ["one","two","three","four","five","six","seven","eight","nine","ten"]
+var topics = ["Chocolate Rain", "Nyan Cat", "Doge","MLG","Bad Luck Brian" ];
+var element_itterator = ["one","two","three","four","five","six","seven","eight","nine","ten"];
 
-var load_buttons = function(){
-	for(var i = 0;i<topics.length;i++){
-		var default_buttons = $("<button>");
-		
-		default_buttons.attr("button-name",topics[i])
-		default_buttons.text(topics[i]);
-		$("#buttons").append(default_buttons);
-	}
-};
+var loadBtn =function(){
+	console.log("loading Buttons......");
 
-load_buttons();
+	 for (var i = 0; i < topics.length; i++) {
 
-	$("#buttons").on("click",function(){
-		// for loop for itterating through all the div's assigning them each one gif
-		for (var i=0;i<10;i++)
-			// make ajax call
-			var topic = $("button-name").val();
-	      	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-	        topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+          // Then dynamicaly generating buttons for each movie in the array.
+          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+          var newButton = $("<button>");
+          // Adding a class
+          newButton.hasClass("buttons");
+          // Providing the button's text with a value of the movie at index i
+          newButton.text(topics[i]);
+          // Adding the button to the HTML
+          $("#buttons").append(newButton);
+        }
+	// for(var i = 0;i<topics.length;i++){
+	// 	var default_buttons = $('<button>');
+	// 	var button_name = topics[i];
+	// 	default_buttons.attr("button-name",button_name);
+	// 	default_buttons.text(button_name);
+	// 	default_buttons.hasClass("buttons");
+	// 	$("#buttons").append(default_buttons);
+	// }
+}
+loadBtn();
 
-	      	$.ajax({
-	          url: queryURL,
-	          method: "GET"
-	        })
-	        .done(function(response){
-	        	console.log(response);
-	        	var search_results = response.data;
-	        	for (var i=0;i< element_itterator.length;i++)
 
-	        		$(`.${element_itterator[i]}`).html("<img src=")
-	        		$(`.${element_itterator[i]}`).append(search_results)
-	        })
-	});
-	// $("#search").on("click", function(){
-	// 	console.log("got in")
-	// 	var search_element = $("#form").val();
-	// 	var new_button = $("<button>");
-	// 	new_button.attr("button-name",search_element)
-	// 	new_button.text(search_element);
-
-	// 	$("#buttons").append(new_button);
-	// });
+	$("#create_button").on("click",function(){
+		var button_attr = $('#form').val();
+		var new_button = $('<button>');
+		new_button.text(button_attr);
+		new_button.hasClass('buttons');
+		$("#buttons").append(new_button);
+});
 
 
 
-
-	$("#search").on("click",function(event){
+	$(".buttons").on("click",function(event){
 			console.log("clicked");
+			$("#buttons").empty();
             var topic = $("#form").val();
             var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             topic + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -62,7 +54,7 @@ load_buttons();
                     var newImg = $('<img>');
                     newImg.addClass('gif');
                     newImg.attr('src', response.data[i].images.fixed_height.url);
-                    $('.gifHolder').prepend(newImg);
+                    $('.gifHolder'+i).prepend(newImg);
                 }
             })
     });
